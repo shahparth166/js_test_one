b75f4adc1ca8d9652885ba8eabcfeebd8558a2d9
Parth Shah <shahparth166@gmail.com>
2026-06-30 18:41:36 +0530

    conversion of data type

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,48 @@
-console.log("Hello parth");
+var name  = 'patrth shah';
+let age = 22;
+names = 'richa15'
+
+// console.log(name);
+// console.log(age);
+// console.log(names);
+
+
+name = 'patrth';
+age = 23;
+names = 'richa16';
+
+// console.table({name, age, names});
+
+// let score = '100';
+// let score = '100abc';
+let score = 'null';
+// console.log(typeof score);
+let newScore = Number(score);
+// console.log(newScore); // print NaN because score is not a valid number.
+// console.log(typeof newScore);  // print number because newScore is a number type but value is NaN.
+
+// "33" => 33 // convert string to number
+// "33abc" => NaN // convert string to number.
+// true => 1 false = 0 // convert boolean to number
+
+let isLoggedIn = 1; // true
+
+let booleanToNumber = Boolean(isLoggedIn);
+// console.log(booleanToNumber); true
+
+let isloggedIn2 = ""; // false
+let booleanToNumber2 = Boolean(isloggedIn2);
+
+// console.log(booleanToNumber2); false
+
+
+let isloggedIn3 = "parth"; // true
+let booleanToNumber3 = Boolean(isloggedIn3);
+
+// console.log(booleanToNumber3); true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+// console.log(stringNumber); // "33"
+// console.log(typeof stringNumber); 
+
