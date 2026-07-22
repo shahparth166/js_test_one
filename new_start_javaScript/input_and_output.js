84171dbc9d0268f950e64346462c47dbf59a6119
Parth Shah <shahparth166@gmail.com>
2026-07-22 15:14:03 +0530

    input and output

---
+++ new_start_javaScript/input_and_output.js
@@ -0,0 +1,38 @@
+// let name = prompt("What is your name?");
+// console.log("Hello " + name);
+
+
+// Get user's name using prompt()
+// Show: "Hello [name]!" using alert()
+// Also print to console using console.log()
+
+// Example:
+// User enters: "Parth"
+// Alert shows: "Hello Parth!"
+// Console shows: "Hello Parth!"
+
+// let userName = prompt("What is your name?");
+// alert("Hello " + userName + "!");
+// console.log("Hello " + userName + "!");
+
+
+// Get user's birth year using prompt()
+// Calculate current age (assume current year is 2026)
+// Show result using alert()
+
+// Example:
+// User enters: 2000
+// Alert shows: "You are 26 years old!"
+// Also print to console
+
+// let birthYear = prompt("What is your birth year?");
+// let currentAge = 2026 - birthYear;
+// alert("You are " + currentAge + " years old!");
+// console.log("You are " + currentAge + " years old!");
+
+
+
+
+
+
+
